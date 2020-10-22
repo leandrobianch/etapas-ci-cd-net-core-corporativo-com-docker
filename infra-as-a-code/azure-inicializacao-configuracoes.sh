e9be3156eb570980c2013716b6adc6e8bcc2a761
unknown <leandrobianch@gmail.comm>
2020-10-21 22:23:35 -0300

    correção do menu do cliente

--- infra-as-a-code/azure-inicializacao-configuracoes.sh
+++ infra-as-a-code/azure-inicializacao-configuracoes.sh
@@ -9,25 +9,63 @@ else
     . ./infra-as-a-code/configurations-rotina.dev.secrets
 fi
 
-echo "Definir configurações do projeto"
+echo "Definindo as configurações do projeto"
 az devops configure --defaults organization=$AZURE_DEVOPS_ORGANIZACAO project=$AZURE_DEVOPS_PROJETO
 
 
 echo "Fazendo login"
 echo $AZURE_DEVOPS_TOKEN | az devops login --organization $AZURE_DEVOPS_ORGANIZACAO
 
+echo "Criando um projeto"
+az devops project create --name $AZURE_DEVOPS_PROJETO \
+                         --description "Teste azure devops CLI" \
+                         --detect false \
+                         --org $AZURE_DEVOPS_ORGANIZACAO \
+                         --source-control git \
+                         --visibility private
+
+#az devops service-endpoint list --project $AZURE_DEVOPS_PROJETO
+echo "Criando um Service Connection com github"
+export GIT_HUB_SERVICE_CONNECT="GitHub-LeandroBianch"
+SERVICE_CONNECTION_JSON=$(az devops service-endpoint github create --github-url "${GIT_HUB_URL}/etapas-ci-cd-net-core-corporativo-com-docker" \
+                                         --name ${GIT_HUB_SERVICE_CONNECT} \
+                                         --detect false \
+                                         --org $AZURE_DEVOPS_ORGANIZACAO \
+                                         --project $AZURE_DEVOPS_PROJETO)
+
+SERVICE_CONNECTION_ID=$(echo $SERVICE_CONNECTION_JSON | ./tools/jq.exe '. | {id : .id, name: .name }' | grep -Po '(?<="id": ").*(?=",)')
+
+#echo $(cat ./tools/teste.json) | ./tools/jq.exe '. | {id : .id}'
+echo "Criando Pipeline Azure Build - CI/CD"
+az pipelines create --name build-ci-cd \
+                    --branch master \
+                    --description "Pipeline criado via cli" \
+                    --detect false \
+                    --org $AZURE_DEVOPS_ORGANIZACAO \
+                    --project $AZURE_DEVOPS_PROJETO \
+                    --repository "${GIT_HUB_URL}/etapas-ci-cd-net-core-corporativo-com-docker" \
+                    --repository-type github \
+                    --service-connection $SERVICE_CONNECTION_ID \
+                    --skip-first-run true \
+                    --yaml-path ./infra-as-a-code/azure/azure-pipelines.yml
+
 
 ARQUIVO_SECURITY="./infra-as-a-code/azure/service-connect.azure-secret.json"
 
-if [ -z $ARQUIVO_SECURITY ] 
-then
-  echo "Removendo o arquivo security $ARQUIVO_SECURITY"
-  rm $ARQUIVO_SECURITY
-fi
+function removerArquivoSecrets {
+  echo "Tentando excluir"
+  if [ -f $ARQUIVO_SECURITY ] 
+  then
+    echo "Removendo o arquivo security $ARQUIVO_SECURITY"
+    rm $ARQUIVO_SECURITY
+  fi
+}
+
+removerArquivoSecrets
 
 echo "Realizando o replace token do service connect azure, gerando o arquivo  token do service connect azure $ARQUIVO_SECURITY"
 
-cat ./infra-as-a-code/azure/service-connect.azure.json | \
+cat ./infra-as-a-code/azure/service-connect-dockerhub.azure.json | \
 sed 's/$USERNAME/'"${DOCKER_REGISTRY_SERVER_USERNAME}"'/' | \
 sed 's/$PASSWORD/'"${DOCKER_REGISTRY_SERVER_PASSWORD}"'/' | \
 sed 's/$REGISTRY_URL/'"${DOCKER_REGISTRY_SERVER_URL}"'/'  | \
@@ -37,20 +75,22 @@ sed 's/$DESCRIPTION/'"${AZURE_DEVOPS_DESCRIPTION_SERVICE_CONNECT}"'/' >> $ARQUIV
 echo "Criando service connect com docker hub"
 az devops service-endpoint create --service-endpoint-configuration $ARQUIVO_SECURITY
 
-echo "Removendo o arquivo security $ARQUIVO_SECURITY"
-if [ -z $ARQUIVO_SECURITY ] 
-then
-  echo "Removendo o arquivo security $ARQUIVO_SECURITY"
-  rm $ARQUIVO_SECURITY
-fi
+# az devops service-endpoint update --id
+#                                   --detect false \
+#                                   --enable-for-all true \
+#                                   --org 
+#                                   --project
 
-echo "Criando grupo de vari connect com docker hub"
+
+removerArquivoSecrets
+
+echo "Criando grupo de variaveis connect com docker hub"
 az pipelines variable-group create \
 --org $AZURE_DEVOPS_ORGANIZACAO \
 --project $AZURE_DEVOPS_PROJETO \
---name ${APPLICATION_NAME} \
---variables cat ./infra-as-a-code/azure/variable-group.secrets \
+--name ${AZURE_DEVOPS_VARIABLE_GROUP} \
+--variables $(cat ./infra-as-a-code/azure/variable-group.secrets) \
 --authorize true \
---description "Criação de variaveis para o projeto ${APPLICATION_NAME}" 
+--description "Criação de variaveis para o projeto ${AZURE_DEVOPS_VARIABLE_GROUP}"
                                    
-                                   
+ 
